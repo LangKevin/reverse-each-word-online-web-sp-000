3d6408b26d82cba38928207386337ff06c665699
kevin lang <langkevina@gmail.com>
2019-05-25 14:45:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
 def reverse_each_word(str)
-  # newArr = str.scan
-  # # array = []
-  # newStr = ''
-  # newArr.collect do |item|
-  #   newStr = newStr + item.reverse
-  # end
-  #
-  # return newStr
-  idx = 0
+  newArr = str.split(" ")
+  # array = []
   newStr = ''
-  while idx < str.length
-    newStr = str[idx] + newStr
-    idx += 1
+  newArr.collect do |item|
+    newStr = newStr + item.reverse
   end
-  return newStr 
+  
+  return newStr
+  # idx = 0
+  # newStr = ''
+  # while idx < str.length
+  #   newStr = str[idx] + newStr
+  #   idx += 1
+  # end
+  # return newStr
 end
