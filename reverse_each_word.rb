69bded59bde5c300dbabc2788d61fce0ad4a67a0
kevin lang <langkevina@gmail.com>
2019-05-25 14:25:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(str)
-  newStr = str.scan /\w+/
+  newArr = str.scan /\w+/
   # array = []
   # array.reverse.collect do |item|
   #   item
   # end
 
-  return newStr
+  return newArr
 end
