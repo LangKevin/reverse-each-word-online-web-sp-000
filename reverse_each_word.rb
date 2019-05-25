9d634c9dd0992cd69a171cc0c8b756bdce62e865
kevin lang <langkevina@gmail.com>
2019-05-25 14:59:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(str)
   newStr = ''
   cnt = 1
   newArr.collect do |item|
-    if cnt < newArr.size - 1
+    if cnt < newArr.size - 2
       newStr = newStr + item.reverse + " "
     else
       newStr = newStr + item.reverse
